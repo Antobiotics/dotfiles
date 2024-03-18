86169d7ab5ed9ce5d5c8405676cc5bfac9ee64fe
gregoire <gregoire.lejay@gmail.com>
2024-03-18 17:15:00 +0100

    🔧

--- nvim/lua/plugins/configs/quarto_conf.lua
+++ nvim/lua/plugins/configs/quarto_conf.lua
@@ -3,12 +3,12 @@ require("quarto").setup({
     closePreviewOnExit = true,
     codeRunner = {
         enabled = true,
-        default_method = "slime", -- 'molten' or 'slime'
+        default_method = "slime", -- 'slime' or 'slime'
         ft_runners = {
             python = "slime",
             r = "slime",
             bash = "slime",
-        },                      -- filetype to runner, ie. `{ python = "molten" }`.
+        }, -- filetype to runner, ie. `{ python = "molten" }`.
         -- Takes precedence over `default_method`
         never_run = { "yaml" }, -- filetypes which are never sent to a code runner
     },
@@ -34,14 +34,30 @@ require("quarto").setup({
 })
 
 vim.keymap.set("n", "<leader>qp", ":lua require'quarto'.quartoPreview()<cr>")
-vim.keymap.set(
-    "n",
-    "<leader>qpq",
-    ":lua require'quarto'.quartoClosePreview()<cr>"
-)
+vim.keymap.set("n", "<leader>qpq", ":lua require'quarto'.quartoClosePreview()<cr>")
 vim.keymap.set("n", "<leader>qrr", ":QuartoSendAbove<cr>")
 vim.keymap.set("n", "<leader>qra", ":QuartoSendAll<cr>")
 vim.keymap.set("n", "<leader>qrb", ":QuartoSendBelow<cr>")
 vim.keymap.set("n", "<leader>qrl", ":QuartoSendLine<cr>")
 vim.keymap.set("n", "<leader>or", "o```{r}<cr>```<esc>O")
 vim.keymap.set("n", "<leader>op", "o```{python}<cr>```<esc>O")
+
+local runner = require("quarto.runner")
+vim.keymap.set("n", "<localleader>rc", runner.run_cell, { desc = "run cell", silent = true })
+vim.keymap.set(
+    "n",
+    "<localleader>ra",
+    runner.run_above,
+    { desc = "run cell and above", silent = true }
+)
+vim.keymap.set("n", "<localleader>rA", runner.run_all, { desc = "run all cells", silent = true })
+vim.keymap.set("n", "<localleader>rl", runner.run_line, { desc = "run line", silent = true })
+vim.keymap.set(
+    "v",
+    "<localleader>r",
+    runner.run_range,
+    { desc = "run visual range", silent = true }
+)
+vim.keymap.set("n", "<localleader>RA", function()
+    runner.run_all(true)
+end, { desc = "run all cells of all languages", silent = true })

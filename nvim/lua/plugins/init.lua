c7abbe6dc1f8450d989a3448bfeb08ae3cceddb9
gregoire <gregoire.lejay@gmail.com>
2024-04-03 11:04:49 +0200

    :construction: trying out neogit

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -324,7 +324,6 @@ require("lazy").setup({
         config = true,
     },
     -- mini
-    -- { "tpope/vim-commentary" },
     { "tpope/vim-surround" },
     { "jamessan/vim-gnupg" },
 
@@ -407,26 +406,18 @@ require("lazy").setup({
     "psf/black",
 
     -- Github
-
-    -- "tpope/vim-fugitive",
+    --
     {
-        "kdheepak/lazygit.nvim",
-        cmd = {
-            "LazyGit",
-            "LazyGitConfig",
-            "LazyGitCurrentFile",
-            "LazyGitFilter",
-            "LazyGitFilterCurrentFile",
-        },
-        -- optional for floating window border decoration
+        "NeogitOrg/neogit",
         dependencies = {
-            "nvim-lua/plenary.nvim",
+            "nvim-lua/plenary.nvim", -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+
+            -- Only one of these is needed, not both.
+            "nvim-telescope/telescope.nvim", -- optional
+            "ibhagwan/fzf-lua", -- optional
         },
-        -- setting the keybinding for LazyGit with 'keys' is recommended in
-        -- order to load the plugin when the command is run for the first time
-        keys = {
-            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-        },
+        config = true,
     },
 
     -- REPL

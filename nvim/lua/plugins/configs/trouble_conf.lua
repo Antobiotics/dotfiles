c253ac3aae9838781612dd9cb5a0a92e58b54e31
gregoire <gregoire.lejay@gmail.com>
2024-08-28 09:44:49 +0200

    💅 slowly coming back to basic bindings

--- nvim/lua/plugins/configs/trouble_conf.lua
+++ nvim/lua/plugins/configs/trouble_conf.lua
@@ -2,12 +2,21 @@ local trouble = require("trouble")
 
 trouble.setup({
     auto_jump = false,
+    auto_preview = false,
     focus = true,
+    keys = {
+        ["<c-x>"] = "jump_split",
+    },
     preview = {
-        type = "split",
-        relative = "win",
-        position = "right",
-        size = 0.5,
+        type = "float",
+        relative = "editor",
+        border = "rounded",
+        title = "preview",
+        title_pos = "center",
+        position = { 0, -3 },
+        size = { width = 0.5, height = 0.55 },
+        zindex = 200,
+        -- size = 0.5,
     },
     win = {
         size = { height = 0.35 },
